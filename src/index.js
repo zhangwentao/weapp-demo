a89c53b274830b0e8ed17957d20b95bfd4ca539a
wentao.zhang <taowenzhang@gmail.com>
2017-12-20 00:15:48 +0800

    import style from js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'Lodash';
+import './style.css';
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
   return element;
 }
 
